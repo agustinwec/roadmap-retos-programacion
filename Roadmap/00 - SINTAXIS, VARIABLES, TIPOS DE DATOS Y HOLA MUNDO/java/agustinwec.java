9d7d4dc59b8d7b4ecadfedcff6288925e291c463
agustinwec <101212599+agustinwec@users.noreply.github.com>
2024-08-05 15:00:26 -0300

    Create agustinwec.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/agustinwec.java
@@ -0,0 +1,21 @@
+public class probando {
+    public static void main(String[] args) {
+        // https://www.java.com/es/
+
+// Este es un comentario en una linea
+
+/* Este es
+  un comentario
+  en varias lineas
+  */
+
+        int numero = 1;
+        long numeroDecimal = 100;
+        double numero1 = 2.5;
+        String cadena = "Hola";
+
+        boolean estoyAprendiendoLogicaDeProgramacion = true;
+
+        System.out.println( cadena + ", estoy programando en java");
+    }
+}
